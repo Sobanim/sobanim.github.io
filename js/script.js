c9c74fe62d44c5e9698450587223b69a58174ca9
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-09-15 22:03:17 +0200

    Added new block. Added animation for numbers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
+var check = 1;
+var target = $('.years, .customers, .workers'); // You counter class
+var targetPos = target.offset().top;
+var winHeight = $(window).height();
+var scrollToElem = targetPos - winHeight;
+
+$(window).scroll(function() {
+  var winScrollTop = $(this).scrollTop();
+  if(winScrollTop > scrollToElem && check) {
+    $('.years, .customers, .workers').each(function() {
+      $(this).prop('Counter', -1).animate({
+        Counter: $(this).text()
+      },{
+        duration: 3000,
+        easing: 'swing',
+        step: function(now) {
+          $(this).text(Math.ceil(now));
+        }
+      });
+    });
+    check = 0;
+  }
+});
+
 $(document).ready(function(){
     $('.burger').click(function(event){
         $('.burger, .mobile-menu__nav').toggleClass('active');
-        // $('.mobile-menu__nav').slideToggle();
     })
 });
