d9c664bc9f988a7cf309d68034a0d5f6f54a80b9
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2020-12-08 01:04:34 +0100

    Add to hosting small game on JS

--- js/script.js
+++ js/script.js
@@ -1,29 +1,101 @@
-var check = 1;
-var target = $('.years, .customers, .workers'); // You counter class
-var targetPos = target.offset().top;
-var winHeight = $(window).height();
-var scrollToElem = targetPos - winHeight;
+let $start = document.querySelector('#start')
+let $game = document.querySelector('#game')
+let $time = document.querySelector('#time')
+let $timeHeader = document.querySelector('#time-header')
+let $resultHeader = document.querySelector('#result-header')
+let $result = document.querySelector('#result')
+let $gameTime = document.querySelector('#game-time')
 
-$(window).scroll(function() {
-  var winScrollTop = $(this).scrollTop();
-  if(winScrollTop > scrollToElem && check) {
-    $('.years, .customers, .workers').each(function() {
-      $(this).prop('Counter', -1).animate({
-        Counter: $(this).text()
-      },{
-        duration: 3000,
-        easing: 'swing',
-        step: function(now) {
-          $(this).text(Math.ceil(now));
+
+
+let score = 0
+let isGameStarted = false
+
+$start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
+
+function startGame() {
+    score = 0
+    setGameTime()
+    $gameTime.setAttribute('disabled', 'true')
+    isGameStarted = true
+    $game.style.backgroundColor = 'white'
+    $start.classList.add('hide')
+
+    let interval = setInterval(function () {
+        let time = parseFloat($time.textContent)
+
+        if(time <= 0){
+            clearInterval(interval)
+            endGame()
+        }else{
+            $time.textContent = (time - 0.1).toFixed(1)
         }
-      });
-    });
-    check = 0;
-  }
-});
+    }, 100)
 
-$(document).ready(function(){
-    $('.burger').click(function(event){
-        $('.burger, .mobile-menu__nav').toggleClass('active');
-    })
-});
+    renderBox()
+}
+
+function setGameScore() {
+    $result.textContent = score.toString()
+}
+
+function setGameTime() {
+    let time = +$gameTime.value
+    $time.textContent = time.toFixed(1)
+    show($timeHeader)
+    hide($resultHeader)
+}
+
+function endGame() {
+    isGameStarted = false
+    setGameScore()
+    $gameTime.removeAttribute('disabled')
+    show($start)
+    $game.innerHTML = ''
+    $game.style.backgroundColor = '#ccc'
+    hide($timeHeader)
+    show($resultHeader)
+}
+
+function handleBoxClick(event) {
+    if(!isGameStarted){
+        return
+    }
+   if(event.target.dataset.box){
+       score++
+       renderBox()
+   }
+}
+
+function renderBox() {
+    $game.innerHTML = ''
+    let box = document.createElement('div')
+    let boxSize = getRandom(20, 100)
+    let gameSize = $game.getBoundingClientRect()
+    let maxTop = gameSize.height - boxSize
+    let maxLeft = gameSize.width - boxSize
+
+    box.style.height = box.style.width = boxSize + 'px'
+    box.style.position = 'absolute'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
+    box.style.backgroundColor = 'black'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box', 'true')
+
+    $game.insertAdjacentElement('afterbegin', box)
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function show($el) {
+    $el.classList.remove('hide')
+}
+
+function hide($el) {
+    $el.classList.add('hide')
+}
