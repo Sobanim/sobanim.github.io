d098d7533e84cd9e5f34310862862f1a1ec9b560
Dmytro Soboliev <sobolev.dmirtiy@gmail.com>
2021-05-17 10:47:21 +0200

    Update gutbliss

--- Projects/gutbliss/script/script.js
+++ Projects/gutbliss/script/script.js
@@ -1,19 +1,5 @@
-// For menu-submenu
-// If mobile
-let isMobile = {
-    Android: function () { return navigator.userAgent.match(/Android/i) },
-    BlackBerry: function () { return navigator.userAgent.match(/Blackberry/i) },
-    iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i) },
-    Opera: function () { return navigator.userAgent.match(/Opera Mini/i) },
-    Windows: function () { return navigator.userAgent.match(/IEMobile/i) },
-    any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows())}
-}
-let body = document.querySelector('body')
+document.querySelector('.burger').addEventListener('click', function () {
+    alert('Mobile menu add in future :)')
+})
 
-if(isMobile.any()){
-    body.classList.add('touch')
 
-    let arrow = document.querySelector('.arrow')
-} else{
-    body.classList.add('mouse')
-}
